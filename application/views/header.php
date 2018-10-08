66d26a350c3401cab85c5fc8bd89089940d6c5c2
Willford <1506676573@qq.com>
2018-10-08 22:55:45 +0200

    Fix: update header, remain a little problem

--- application/views/header.php
+++ application/views/header.php
@@ -8,50 +8,67 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <title>Project Tool</title>
     <style>
         body{
-            background-color: #85d3c0;
+            background-color:#dce0db;
         }
-        
-        .welcome-title-position{
-            margin-top: 140px;
-        }
-        .container{
-            padding : 15px;
-        }
-        .project{
-            background-color : #fff;
-            border-radius : .25rem;
-            overflow : hidden;
-            box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+        .navbar{
+            margin-bottom: 25px;
         }
     </style>
     <script src="<?=asset('js/jquery.min.js')?>"> </script>
     <script src="<?=asset('js/bootstrap.js')?>"> </script>
 </head>
 <header>
-<nav class="navbar navbar-light bg-light">
-            <a class="navbar-brand"><h2>Project tool</h2></a>
-            
-            <div class="text-right account-mangage-position">
-                
+<nav class="navbar navbar-expand-lg bg-success navbar-dark">
+        <div class="container">
+            <a class="navbar-brand" href="home.html"> PROJECTS TOOL </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- <ul class="navbar-nav mr-auto">
+                </ul> -->
+            <?php
+                if(isset($_SESSION["user_connected"]))
+                {?>
+                    <ul class="navbar-nav ml-auto">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="dropdownMenunotify" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fas fa-bell"></i> <sup> <span class="badge badge-danger"> 5 </span> </sup>
+                                </a>
+                                        
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenunotify">
+                                    <a class="dropdown-item" href="#"> Notification one </a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#"> Notification two </a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#"> Notification three </a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="fas fa-user"></i> Teo Lambert
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#"> User Profile </a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="index.html"> Logout </a>
+                                </div>
+                            </li>
+                        </ul>
 
-    <?php
-        if(isset($_SESSION["user_connected"]))
-        {?>
-            <a class="btn btn-outline-success" href="<?=base_url()?>Project/index"> My projects </a>
-            <span> &nbsp; &nbsp; &nbsp; &nbsp; </span>
-            <a class="btn btn-outline-success" href="<?=base_url()?>Registration/logout"> Logout </a>
-
-        <?php 
-        }
-        else
-        { ?>
-                    <a class="btn btn-outline-success" href="<?=base_url()?>Registration/account_creation"> Create an account </a>
-                    <span> &nbsp; &nbsp; &nbsp; &nbsp; </span>
-                    <a class="btn btn-outline-success" href="<?=base_url()?>Registration/user_login"> Login </a>
-       <?php } ?>
-
-                   </div>
-        </nav>
+                <?php 
+                }
+                else
+                { ?>
+                <!-- TODO: modify the create account and login btn-->
+                            <a class="btn btn-outline-success" href="<?=base_url()?>Registration/account_creation"> Create an account </a>
+                            <span> &nbsp; &nbsp; &nbsp; &nbsp; </span>
+                            <a class="btn btn-outline-success" href="<?=base_url()?>Registration/user_login"> Login </a>
+            <?php } ?>
+            </div>
+        </div>
+    </nav>
 </header>
 <head>
     <?php if(!isset($_SESSION["user_connected"])) { ?>
