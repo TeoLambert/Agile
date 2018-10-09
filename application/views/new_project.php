8ccc5980565ed3a728962aa342b58cfcd02c831d
Willford <1506676573@qq.com>
2018-10-09 22:06:02 +0200

    Feature: Integrate new_project page

--- application/views/new_project.php
+++ application/views/new_project.php
@@ -1,32 +1,47 @@
-<h2>Create a new project</h2>
-<div id="project">
-    <form name="new_project" method="post" action="<?=base_url()?>/Project/project_creation">
-    <div id="pro_name">
-        <label for="pro_name">Project name : </label>
-        <input type="text" name="pro_name">
-    </div>
-    <div id="pro_deadline">
-        <label for="pro_deadline">Deadline : </label>
-        <input type="date" name="pro_deadline">
-    </div>
-    <div id="pro_customer">
-        <label for="pro_customer">Customer name : </label>
-        <input type="text" name="pro_customer">
-    </div>
-    <div id="pro_customer_tel">
-        <label for="pro_customer_tel">Customer phone : </label>
-        <input type="text" name="pro_customer_tel">
-    </div>
-    <div id="pro_customer_mail">
-        <label for="pro_customer_mail">Customer mail : </label>
-        <input type="mail" name="pro_customer_mail">
-    </div>
-    <div id="pro_desc">
-        <label for="pro_desc">Project description : </label>
-        <textarea name="pro_desc"></textarea>
-    </div>
-    <div id="submit">
-        <input type="submit" name="submit" value="Create the project">
-    </div>
-</div>
+<div class="col-lg-9">
+                <div class="card">
+                    <div class="card-header"> CREATE PROJECT </div>
+                    <div class="card-body">
+                        <form action="<?=base_url()?>/Project/project_creation" method="POST">
+                            <div class="form-group">
+                                <div class="form-row">
+                                    <div class="col-lg-6">
+                                        <label for="projectName"> Project Name: </label>
+                                        <input type="text" name="pro_name" id="projectName" class="form-control" required>
+                                    </div>
+                                    <div class="col-lg-6">
+                                        <label for="projectDeadline"> Project Deadline: </label>
+                                        <input type="date" name="pro_deadline" id="projectDeadline" class="form-control" required>
+                                    </div>
+                                </div>
+                            </div>
 
+                            <div class="form-group">
+                                <div class="form-row">
+                                    <div class="col-lg-4">
+                                        <label for="customerName"> Customer Name: </label>
+                                        <input type="text" name="pro_customer" id="customerName" class="form-control" required>
+                                    </div>
+                                    <div class="col-lg-3">
+                                        <label for="customerPhone"> Customer Phone: </label>
+                                        <input type="text" name="pro_customer_tel" id="customerPhone" class="form-control" required>
+                                    </div>
+                                    <div class="col-lg-5">
+                                        <label for="customerEmail"> Customer E-mail: </label>
+                                        <input type="email" name="pro_customer_mail" id="customerEmail" class="form-control" required>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="projectDescription"> Project Description: </label>
+                                <textarea class="form-control" name="pro_desc" id="projectDescription" cols="30" rows="3" required></textarea>
+                            </div>
+
+                            <div class="form-group text-right">
+                                <button type="submit" name="submit" class="btn btn-outline-success"> Create Project </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
