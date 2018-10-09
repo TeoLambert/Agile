c3b3d7539345912dbf7a520d447166f58812ecad
Willford <1506676573@qq.com>
2018-10-09 20:43:15 +0200

    Feature: integrate side_bar

---
+++ application/views/side_bar.php
@@ -0,0 +1,8 @@
+<div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="list-group">
+                        <a class="list-group-item list-group-item-action" href="<?=base_url()?>Registration/account_creation"> <i class="fas fa-project-diagram"></i> Projects </a>
+                        <a class="list-group-item list-group-item-action" href="<?=base_url()?>Registration/account_creation"> <i class="fas fa-tasks"></i> My Tasks </a>
+                </div>
+            </div>
