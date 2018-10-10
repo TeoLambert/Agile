5713ad4e85e5155cd7cfabdd2e9506153b288e58
Willford <1506676573@qq.com>
2018-10-10 21:21:07 +0200

    Fix: modified hrefs in card navs

--- application/views/projects.php
+++ application/views/projects.php
@@ -4,10 +4,10 @@
                         <h4 class="text-info text-center"><?= $project[0]->pro_name?></h4>
                         <nav>
                             <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                                <a class="nav-item nav-link active" id="nav-description-tab" data-toggle="tab" href="#nav-description" role="tab" aria-controls="nav-description" aria-selected="true"> Description </a>
-                                <a class="nav-item nav-link" id="nav-backlog-tab" data-toggle="tab" href="#nav-backlog" role="tab" aria-controls="nav-backlog" aria-selected="false"> Backlog </a>
-                                <a class="nav-item nav-link" id="nav-board-tab" data-toggle="tab" href="#nav-board" role="tab" aria-controls="nav-board" aria-selected="false"> Board </a>
-                                <a class="nav-item nav-link" id="nav-people-tab" data-toggle="tab" href="#nav-people" role="tab" aria-controls="nav-people" aria-selected="false"> People </a>
+                                <a class="nav-item nav-link active" id="nav-description-tab" data-toggle="tab" href="<?=base_url()?>Projects/projects/<?=$project[0]->pro_id?>" role="tab" aria-controls="nav-description" aria-selected="true"> Description </a>
+                                <a class="nav-item nav-link" id="nav-backlog-tab" data-toggle="tab" href="<?=base_url()?>Projects/show_req/<?=$project[0]->pro_id?>" role="tab" aria-controls="nav-backlog" aria-selected="false"> Backlog </a>
+                                <a class="nav-item nav-link" id="nav-board-tab" data-toggle="tab" href="<?=base_url()?>Projects/show_task/<?=$project[0]->pro_id?>" role="tab" aria-controls="nav-board" aria-selected="false"> Board </a>
+                                <a class="nav-item nav-link" id="nav-people-tab" data-toggle="tab" href="<?=base_url()?>Projects/show_worker/<?=$project[0]->pro_id?>" role="tab" aria-controls="nav-people" aria-selected="false"> People </a>
                             </div>
                         </nav>
                         <div class="tab-content" id="nav-tabContent">
