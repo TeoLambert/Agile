1d9a35612cf453c180d87149e7db39f84ce14516
Willford <1506676573@qq.com>
2018-10-10 23:10:43 +0200

    Fix: change task model

--- application/models/Task_model.php
+++ application/models/Task_model.php
@@ -6,6 +6,8 @@ class Task_model extends CI_Model {
     public $tas_name;
     public $tas_deadline;
     public $tas_desc;
+    public $tas_progress;
+    public $tas_priority;
     public $pro_id;
 
     public function __construct()
